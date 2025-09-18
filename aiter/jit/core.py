e2cf209c76c6a4b20088979010569f61085ba24b
ZhangLirong <Lirong.Zhang@amd.com>
2025-09-18 14:18:01 +0800

    Using dummy when input without tensor (#1016)

--- aiter/jit/core.py
+++ aiter/jit/core.py
@@ -925,11 +925,25 @@ def compile_ops(
         import inspect
 
         sig = inspect.signature(func)
+        input_is_tensor = False
+        parameters = list(sig.parameters.values())
+
+        if parameters:
+            first_param = parameters[0]
+            if (
+                first_param.annotation is not inspect.Parameter.empty
+                and first_param.annotation is torch.Tensor
+            ):
+                input_is_tensor = True
+
         input_part, output_part = schema.split("->", 1)
-        if not sig.parameters:
-            new_input = "(Tensor dummy)"
+        if input_is_tensor:
+            new_input = input_part
         else:
-            new_input = "(Tensor dummy, " + input_part[1:]
+            if not sig.parameters:
+                new_input = "(Tensor dummy)"
+            else:
+                new_input = "(Tensor dummy, " + input_part[1:]
 
         return_int = False
         return_annotation = sig.return_annotation
@@ -941,19 +955,39 @@ def compile_ops(
 
         loadName = func.__name__
 
-        def abstract_impl(dummy, *args, custom_build_args={}, **kwargs):
+        def abstract_impl(*args, custom_build_args={}, **kwargs):
             if return_int:
                 return torch.empty(1, device="cuda"), 1
             if gen_fake is not None:
                 return gen_fake(*args, **kwargs)
             return func(*args, **kwargs)
 
-        def outer_wrapper(dummy, *args, **kwargs):
+        def outer_wrapper(*args, **kwargs):
             return (
                 wrapper(*args, **kwargs)
                 if not return_int
                 else (torch.empty(1, device="cuda"), wrapper(*args, **kwargs))
             )
+
+        def abstract_impl_dummy(dummy, *args, custom_build_args={}, **kwargs):
+            if return_int:
+                return torch.empty(1, device="cuda"), 1
+            if gen_fake is not None:
+                return gen_fake(*args, **kwargs)
+            return func(*args, **kwargs)
+
+        def outer_wrapper_dummy(dummy, *args, **kwargs):
+            return (
+                wrapper(*args, **kwargs)
+                if not return_int
+                else (torch.empty(1, device="cuda"), wrapper(*args, **kwargs))
+            )
+
+        custom_func = outer_wrapper
+        fake_func = abstract_impl
+        if not input_is_tensor:
+            custom_func = abstract_impl_dummy
+            fake_func = outer_wrapper_dummy
 
         if not hasattr(torch.ops.aiter, f"wrapper_{loadName}"):
             if is_torch_equal_or_newer("2.8.0"):
@@ -963,17 +997,20 @@ def compile_ops(
             op_schema = f"aiter::wrapper_{loadName}" + schema
             aiter_lib.define(op_schema, tags=tags)
             aiter_lib.impl(
-                f"aiter::wrapper_{loadName}", outer_wrapper, dispatch_key="CUDA"
+                f"aiter::wrapper_{loadName}", custom_func, dispatch_key="CUDA"
             )
             aiter_lib.impl(
-                f"aiter::wrapper_{loadName}", outer_wrapper, dispatch_key="CPU"
+                f"aiter::wrapper_{loadName}", custom_func, dispatch_key="CPU"
             )
-            aiter_lib._register_fake(f"wrapper_{loadName}", abstract_impl)
+            aiter_lib._register_fake(f"wrapper_{loadName}", fake_func)
 
         def wrapper_custom(*args, custom_build_args={}, **kwargs):
-            dummy = torch.empty(1, device="cuda")
-            result = getattr(torch.ops.aiter, f"wrapper_{loadName}")(
-                dummy, *args, **kwargs
+            result = (
+                getattr(torch.ops.aiter, f"wrapper_{loadName}")(*args, **kwargs)
+                if input_is_tensor
+                else getattr(torch.ops.aiter, f"wrapper_{loadName}")(
+                    torch.empty(1, device="cuda"), *args, **kwargs
+                )
             )
             return result[1] if return_int else result
 
