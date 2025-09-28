9c1ad799b50414b92dd82f71df199e1eb79ee26e
slippedJim <jim.guo@amd.com>
2025-09-28 22:34:47 +0800

    [MI35X] Fix FA cpp api compile error & remove batch stride in FA fwd varlen (#1105)
    * remove batch stride in group mode
    
    * do not import torch when torch exclude
    
    * update
    
    * format
    
    * fix for fa fwd varlen
    
    ---------
    
    Co-authored-by: Xin Huang <Xin.Huang@amd.com>

--- aiter/jit/core.py
+++ aiter/jit/core.py
@@ -353,10 +353,12 @@ def build_module(
         if get_gfx() == "gfx950" and int(os.getenv("AITER_FP4x2", "1")) > 0:
             flags_hip += ["-D__Float4_e2m1fn_x2"]
 
-        import torch
+        if not torch_exclude:
+            import torch
 
-        if hasattr(torch, "float4_e2m1fn_x2"):
-            flags_hip += ["-DTORCH_Float4_e2m1fn_x2"]
+            if hasattr(torch, "float4_e2m1fn_x2"):
+                flags_hip += ["-DTORCH_Float4_e2m1fn_x2"]
+
         flags_cc += flags_extra_cc
         flags_hip += flags_extra_hip
         archs = validate_and_update_archs()
