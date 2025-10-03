9257dcce658013ef7bca7f1944a539c8ba4e2959
lucas-santos-amd <Lucas.Santos@amd.com>
2025-10-03 13:56:17 -0300

    [TRITON] Add SplitK to the GEMM a16w16 Triton Kernel (#923)

--- aiter/ops/triton/_triton_kernels/gemm_a16w16.py
+++ aiter/ops/triton/_triton_kernels/gemm_a16w16.py
@@ -10,7 +10,8 @@ from ..utils.core import AITER_TRITON_CONFIGS_PATH
 
 @triton.heuristics(
     {
-        "EVEN_K": lambda args: args["K"] % args["BLOCK_SIZE_K"] == 0,
+        "EVEN_K": lambda args: (args["K"] % (args["SPLITK_BLOCK_SIZE"]) == 0)
+        and (args["SPLITK_BLOCK_SIZE"] % args["BLOCK_SIZE_K"] == 0),
         "GRID_MN": lambda args: triton.cdiv(args["M"], args["BLOCK_SIZE_M"])
         * triton.cdiv(args["N"], args["BLOCK_SIZE_N"]),
     }
@@ -27,6 +28,7 @@ def _gemm_a16_w16_kernel(
     stride_ak,
     stride_bk,
     stride_bn,
+    stride_ck,
     stride_cm,
     stride_cn,
     # Meta-parameters
@@ -34,6 +36,8 @@ def _gemm_a16_w16_kernel(
     BLOCK_SIZE_N: tl.constexpr,
     BLOCK_SIZE_K: tl.constexpr,
     GROUP_SIZE_M: tl.constexpr,
+    NUM_KSPLIT: tl.constexpr,
+    SPLITK_BLOCK_SIZE: tl.constexpr,
     EVEN_K: tl.constexpr,
     GRID_MN: tl.constexpr,
     cache_modifier: tl.constexpr,
@@ -48,64 +52,148 @@ def _gemm_a16_w16_kernel(
     tl.assume(stride_ak > 0)
     tl.assume(stride_bk > 0)
     tl.assume(stride_bn > 0)
+    tl.assume(stride_ck > 0)
     tl.assume(stride_cm > 0)
     tl.assume(stride_cn > 0)
 
     # -----------------------------------------------------------
     # Map program ids `pid` to the block of C it should compute.
     # This is done in a grouped ordering to promote L2 data reuse.
-    pid = tl.program_id(axis=0)
+    pid_unified = tl.program_id(axis=0)
+    pid_unified = remap_xcd(pid_unified, GRID_MN * NUM_KSPLIT, NUM_XCDS=8)
+    pid_k = pid_unified % NUM_KSPLIT
+    pid = pid_unified // NUM_KSPLIT
     num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
     num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
 
-    remap_xcd(pid, GRID_MN)
-
-    pid_m, pid_n = pid_grid(pid, num_pid_m, num_pid_n, GROUP_SIZE_M=GROUP_SIZE_M)
+    if NUM_KSPLIT == 1:
+        pid_m, pid_n = pid_grid(pid, num_pid_m, num_pid_n, GROUP_SIZE_M=GROUP_SIZE_M)
+    else:
+        pid_m = pid // num_pid_n
+        pid_n = pid % num_pid_n
 
     tl.assume(pid_m >= 0)
     tl.assume(pid_n >= 0)
+    tl.assume(pid_k >= 0)
 
-    # Create pointers for first block of A and B input matrices
-    offs_k = tl.arange(0, BLOCK_SIZE_K)
-    offs_am = (pid_m.to(tl.int64) * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
-    offs_bn = (pid_n.to(tl.int64) * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
-    a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
-    b_ptrs = b_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
+    split_k_start = pid_k * SPLITK_BLOCK_SIZE
+    if split_k_start < K:
+        # Create pointers for first block of A and B input matrices
+        offs_k = tl.arange(0, BLOCK_SIZE_K)
+        offs_k_split = split_k_start + offs_k
+        offs_am = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
+        offs_bn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
 
-    acc_dtype = tl.float32 if c_ptr.type.element_ty != tl.int8 else tl.int32
-    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=acc_dtype)
+        a_ptrs = a_ptr + (
+            offs_am[:, None] * stride_am + offs_k_split[None, :] * stride_ak
+        )
+        b_ptrs = b_ptr + (
+            offs_k_split[:, None] * stride_bk + offs_bn[None, :] * stride_bn
+        )
 
-    for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
-        # Load the next block of A and B, generate a mask by checking the K dimension.
-        # If it is out of bounds, set it to 0.
-        if EVEN_K:
-            a = tl.load(a_ptrs)
-            b = tl.load(b_ptrs, cache_modifier=cache_modifier)
-        else:
-            a = tl.load(a_ptrs, mask=offs_k[None, :] < K - k * BLOCK_SIZE_K, other=0.0)
-            b = tl.load(
-                b_ptrs,
-                mask=offs_k[:, None] < K - k * BLOCK_SIZE_K,
-                other=0.0,
-                cache_modifier=cache_modifier,
-            )
+        acc_dtype = tl.float32 if c_ptr.type.element_ty != tl.int8 else tl.int32
+        accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=acc_dtype)
 
-        accumulator += tl.dot(a, b, input_precision="ieee")
+        split_k_end = tl.minimum(split_k_start + SPLITK_BLOCK_SIZE, K)
+        k_span = split_k_end - split_k_start
+        num_k_iter = tl.cdiv(k_span, BLOCK_SIZE_K)
 
-        # Advance the ptrs to the next K block.
-        a_ptrs += BLOCK_SIZE_K * stride_ak
-        b_ptrs += BLOCK_SIZE_K * stride_bk
+        for k in range(num_k_iter):
+            # Load the next block of A and B, generate a mask by checking the K dimension.
+            # If it is out of bounds, set it to 0.
+            if EVEN_K:
+                a = tl.load(a_ptrs)
+                b = tl.load(b_ptrs, cache_modifier=cache_modifier)
+            else:
+                a = tl.load(
+                    a_ptrs, mask=offs_k[None, :] < k_span - k * BLOCK_SIZE_K, other=0.0
+                )
+                b = tl.load(
+                    b_ptrs,
+                    mask=offs_k[:, None] < k_span - k * BLOCK_SIZE_K,
+                    other=0.0,
+                    cache_modifier=cache_modifier,
+                )
+            accumulator += tl.dot(a, b, input_precision="ieee")
+            # Advance the ptrs to the next K block.
+            a_ptrs += BLOCK_SIZE_K * stride_ak
+            b_ptrs += BLOCK_SIZE_K * stride_bk
+
+        if use_activation and NUM_KSPLIT == 1:
+            accumulator = activation(accumulator)
+
+        # Write back the block of the output matrix C with masks.
+        c = accumulator.to(c_ptr.type.element_ty)
+        offs_cm = pid_m.to(tl.int64) * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+        offs_cn = pid_n.to(tl.int64) * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+        c_ptrs = (
+            c_ptr
+            + stride_cm * offs_cm[:, None]
+            + stride_cn * offs_cn[None, :]
+            + pid_k * stride_ck
+        )
+        c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
+        tl.store(c_ptrs, c, mask=c_mask)
+
+
+@triton.jit
+def _gemm_a16w16_reduce_kernel(
+    c_in_ptr,
+    c_out_ptr,
+    M,
+    N,
+    stride_c_in_k,
+    stride_c_in_m,
+    stride_c_in_n,
+    stride_c_out_m,
+    stride_c_out_n,
+    BLOCK_SIZE_M: tl.constexpr,
+    BLOCK_SIZE_N: tl.constexpr,
+    ACTUAL_KSPLIT: tl.constexpr,
+    MAX_KSPLIT: tl.constexpr,
+    activation: tl.constexpr,
+    use_activation: tl.constexpr,
+):
+
+    tl.assume(stride_c_in_k > 0)
+    tl.assume(stride_c_in_m > 0)
+    tl.assume(stride_c_in_n > 0)
+    tl.assume(stride_c_out_m > 0)
+    tl.assume(stride_c_out_n > 0)
+
+    pid_m = tl.program_id(axis=0)
+    pid_n = tl.program_id(axis=1)
+
+    tl.assume(pid_m > 0)
+    tl.assume(pid_n > 0)
+
+    offs_m = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
+    offs_n = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
+    offs_k = tl.arange(0, MAX_KSPLIT)
+    c_in_ptrs = (
+        c_in_ptr
+        + (offs_k[:, None, None] * stride_c_in_k)
+        + (offs_m[None, :, None] * stride_c_in_m)
+        + (offs_n[None, None, :] * stride_c_in_n)
+    )
+
+    if ACTUAL_KSPLIT == MAX_KSPLIT:
+        c = tl.load(c_in_ptrs)
+    else:
+        c = tl.load(c_in_ptrs, mask=offs_k[:, None, None] < ACTUAL_KSPLIT, other=0.0)
+    c = tl.sum(c, axis=0)
 
     if use_activation:
-        accumulator = activation(accumulator)
-    c = accumulator.to(c_ptr.type.element_ty)
+        c = activation(c)
+    c = c.to(c_out_ptr.type.element_ty)
 
-    # Write back the block of the output matrix C with masks.
-    offs_cm = pid_m.to(tl.int64) * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
-    offs_cn = pid_n.to(tl.int64) * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
-    c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
-    c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
-    tl.store(c_ptrs, c, mask=c_mask)
+    c_out_ptrs = (
+        c_out_ptr
+        + (offs_m[:, None] * stride_c_out_m)
+        + (offs_n[None, :] * stride_c_out_n)
+    )
+
+    tl.store(c_out_ptrs, c)
 
 
 @functools.lru_cache(maxsize=1024)
@@ -136,6 +224,25 @@ def _get_config(
     bounds = [64, 128, 256, 512, 2048]
     for bound in bounds:
         if M <= bound and f"M_LEQ_{bound}" in _get_config._config_dict[key]:
-            return _get_config._config_dict[key][f"M_LEQ_{bound}"]
+            temp_config = _get_config._config_dict[key][f"M_LEQ_{bound}"]
+            break
     else:
-        return _get_config._config_dict[key]["M_GEQ_4096"]
+        temp_config = _get_config._config_dict[key]["M_GEQ_4096"]
+
+    # Copy to avoid mutating the cached config
+    chosen_config = dict(temp_config)
+
+    if "NUM_KSPLIT" not in chosen_config:
+        chosen_config["NUM_KSPLIT"] = 1
+
+    chosen_config["SPLITK_BLOCK_SIZE"] = triton.cdiv(K, chosen_config["NUM_KSPLIT"])
+
+    if chosen_config["BLOCK_SIZE_K"] > chosen_config["SPLITK_BLOCK_SIZE"]:
+        chosen_config["BLOCK_SIZE_K"] = triton.next_power_of_2(
+            chosen_config["SPLITK_BLOCK_SIZE"]
+        )
+        if chosen_config["BLOCK_SIZE_K"] > chosen_config["SPLITK_BLOCK_SIZE"]:
+            chosen_config["BLOCK_SIZE_K"] = chosen_config["BLOCK_SIZE_K"] // 4
+    chosen_config["BLOCK_SIZE_K"] = max(chosen_config["BLOCK_SIZE_K"], 16)
+
+    return chosen_config
