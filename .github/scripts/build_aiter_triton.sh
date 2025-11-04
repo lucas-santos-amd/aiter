16b6e35d661efcf6c14954deff624d16376a6c22
Xin Huang <Xin.Huang@amd.com>
2025-11-04 20:21:44 +0800

    CI: Skip triton setup in Aiter standard/multigpu tests and add retries when setting up triton (#1325)
    * CI: Skip triton in Aiter standard and multigpu tests
    
    * Add retries when building triton
    
    * Add ninja installation

---
+++ .github/scripts/build_aiter_triton.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -ex
+
+echo
+echo "==== ROCm Packages Installed ===="
+dpkg -l | grep rocm || echo "No ROCm packages found."
+
+echo
+echo "==== Install dependencies and aiter ===="
+pip install --upgrade pandas zmq einops numpy==1.26.2
+pip uninstall -y aiter || true
+pip install --upgrade "pybind11>=3.0.1"
+pip install --upgrade "ninja>=1.11.1"
+python3 setup.py develop
+
+# Read BUILD_TRITON env var, default to 1. If 1, install Triton; if 0, skip installation.
+BUILD_TRITON=${BUILD_TRITON:-1}
+
+if [[ "$BUILD_TRITON" == "1" ]]; then
+    echo
+    echo "==== Install triton ===="
+    pip uninstall -y triton || true
+    git clone --depth=1 https://github.com/triton-lang/triton || true
+    cd triton
+    pip install -r python/requirements.txt
+    pip install filecheck
+    MAX_JOBS=64 pip --retries=5 install .
+    cd ..
+else
+    echo
+    echo "[SKIP] Triton installation skipped because BUILD_TRITON=$BUILD_TRITON"
+fi
+
+echo
+echo "==== Show installed packages ===="
+pip list
