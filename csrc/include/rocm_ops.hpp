b2f6ebab0d3b7b84d1ab06817c70980c41dc4b37
Lingpeng Jin <103567126+valarLip@users.noreply.github.com>
2025-11-17 19:58:56 +0800

    topk_per_row_opt (#1394)
    * topk_per_row_opt
    
    * Optimize topk by integrating air-topk kernel (TODO: proper workspace size computation and init) (#1409)
    
    * update
    
    * slightly optmize
    
    * add back
    
    ---------
    
    Co-authored-by: Cu Cui <cu.cui@alumni.uni-heidelberg.de>
    Co-authored-by: carlushuang <carlus.huang@amd.com>

--- csrc/include/rocm_ops.hpp
+++ csrc/include/rocm_ops.hpp
@@ -1337,6 +1337,7 @@ namespace py = pybind11;
           py::arg("rowStarts"),    \
           py::arg("rowEnds"),      \
           py::arg("indices"),      \
+          py::arg("values"),       \
           py::arg("numRows"),      \
           py::arg("stride0"),      \
           py::arg("stride1"));     \
