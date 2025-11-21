b08834e9a8f71abd6cdaf4f056dcad2e5de0ed1e
BingYuan.Zhou <BingYuan.Zhou@amd.com>
2025-11-21 18:06:58 +0800

    cktile weight preshuffle test and auto tuning for a8w8 (#1400)
    * cktile bpreshuffle && tuning code
    
    * refine code
    
    * refine code
    
    * refine code
    
    * refine
    
    * refine
    
    * fix merge conflict
    
    * fix conflict
    
    * fix CI build fail
    
    * refine code
    
    * align aiter interface
    
    * refine
    
    * add get_padded_M
    
    ---------
    
    Co-authored-by: solin <bingzhou@amd.com>
    Co-authored-by: Ying Zhou <Ying.Zhou2@amd.com>

--- csrc/include/rocm_ops.hpp
+++ csrc/include/rocm_ops.hpp
@@ -525,6 +525,29 @@ namespace py = pybind11;
           py::arg("Out"),                 \
           py::arg("kernelId") = 0,        \
           py::arg("splitK")   = 0);
+
+#define GEMM_A8W8_BPRESHUFFLE_CKTILE_PYBIND      \
+    m.def("gemm_a8w8_bpreshuffle_cktile", \
+          &gemm_a8w8_bpreshuffle_cktile,  \
+          "gemm_a8w8_bpreshuffle_cktile", \
+          py::arg("XQ"),                  \
+          py::arg("WQ"),                  \
+          py::arg("x_scale"),             \
+          py::arg("w_scale"),             \
+          py::arg("Out"));
+
+#define GEMM_A8W8_BPRESHUFFLE_CKTILE_TUNE_PYBIND \
+    m.def("gemm_a8w8_bpreshuffle_cktile_tune",   \
+          &gemm_a8w8_bpreshuffle_cktile_tune,    \
+          "gemm_a8w8_bpreshuffle_cktile_tune",   \
+          py::arg("XQ"),                         \
+          py::arg("WQ"),                         \
+          py::arg("x_scale"),                    \
+          py::arg("w_scale"),                    \
+          py::arg("Out"),                        \
+          py::arg("kernelId") = 0,               \
+          py::arg("splitK")   = 0);
+
 #define MHA_BWD_ASM_PYBIND                        \
     m.def("fmha_v3_bwd",                          \
           &aiter::torch_itfs::fmha_v3_bwd,        \
