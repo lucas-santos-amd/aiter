096b7d96f8e21ddf603f76201357187891b2e073
ZhangLirong <lirzhang@amd.com>
2025-12-01 21:03:30 +0800

    Support PA persistent mode in AITER (#1513)
    * draft interface for pa
    
    * modifty get_pa_metadata_v1
    
    * add output and cu kernel
    
    * add pa_meta_data api in cu
    
    * fix bug
    
    * move pa metadata to mla file
    
    * use context_len replace last_kv_lens
    
    * fix some bug and enable pa ps
    
    * fix ut
    
    * support metadata+pa+reduce
    
    * add some file
    
    * fix(pa_ps): add kv varlen, fix kv_indptr, kv_indices, get_pa_metadata_info
    
    Signed-off-by: Double Young <yang.yang2@amd.com>
    
    * fix(pa_ps): isolate & fix gen_pa_metadata
    
    Signed-off-by: Double Young <yang.yang2@amd.com>
    
    * test(pa_ps): comment test_pa_ps_simple
    
    * fix(pa_ps): add gqa10 support in mla_reduce dispatch
    
    Signed-off-by: Double Young <yang.yang2@amd.com>
    
    * add new ut for pa ps
    
    * fix(pa_ps): fix calc_LSE and replace asm_pa_ps
    
    Signed-off-by: Double Young <yang.yang2@amd.com>
    
    * test(pa_ps): add load & dump metadata
    
    Signed-off-by: Double Young <yang.yang2@amd.com>
    
    * tmp(pa_ps): hack mla_reduce to ignore nan
    
    Signed-off-by: Double Young <yang.yang2@amd.com>
    
    * add q_tile
    
    * add all case and co
    
    * fix some format
    
    * fix(pa_ps): fix get num_reduce_tile in mla_reduce
    
    Signed-off-by: Double Young <yang.yang2@amd.com>
    
    * add p99 for performance
    
    * fix(pa_ps): fix mla_reduce for gqa10
    
    Signed-off-by: Double Young <yang.yang2@amd.com>
    
    * fix(pa_ps): decrease kFixedOverheadNumBlocks for perf
    
    Signed-off-by: Double Young <yang.yang2@amd.com>
    
    * fix(pa_ps): fix metadata init
    
    Signed-off-by: Double Young <yang.yang2@amd.com>
    
    * fix pa acc issue
    
    * format ut output
    
    * add gfx950 co
    
    * replace new 950 co
    
    ---------
    
    Signed-off-by: Double Young <yang.yang2@amd.com>
    Co-authored-by: Double Young <yang.yang2@amd.com>

--- csrc/include/mla.h
+++ csrc/include/mla.h
@@ -70,3 +70,21 @@ void mla_reduce_v1(const torch::Tensor& partial_output,
                    const torch::Tensor& reduce_partial_map,
                    torch::Tensor& final_output,
                    std::optional<torch::Tensor>& final_lse);
+
+void get_pa_metadata_v1(const torch::Tensor& seqlens_qo_indptr, // [batch size + 1]
+                        const torch::Tensor& seqlens_kv_indptr, // [batch size + 1]
+                        const int32_t num_heads_per_head_k,
+                        const int32_t num_heads_k,
+                        const bool is_causal,
+                        torch::Tensor& work_metadata_ptrs,
+                        torch::Tensor& work_info_set,
+                        torch::Tensor& work_indptr,
+                        torch::Tensor& reduce_indptr,
+                        torch::Tensor& reduce_final_map,
+                        torch::Tensor& reduce_partial_map,
+                        const int32_t kv_granularity,
+                        const int32_t max_seqlen_qo,
+                        const int32_t uni_seqlen_qo,
+                        const bool fast_mode,
+                        const int32_t topk,
+                        const int32_t max_split_per_batch);
--- csrc/kernels/mla/metadata.cu
+++ csrc/kernels/mla/metadata.cu
@@ -5,6 +5,7 @@
 #include "metadata/v1_1_device.cuh"
 #include "metadata/v1_1_host.cuh"
 #include "metadata/v1_2_device.cuh"
+#include "metadata/v1_2_pa_device.cuh"
 
 // ===================================================================================================================
 // MLA Metadata V1
@@ -139,4 +140,57 @@ std::vector<torch::Tensor> get_mla_metadata_v1_no_redundant(
         is_causal,
         kv_granularity,
         true);
+}
+
+
+void get_pa_metadata_v1(
+    const torch::Tensor& seqlens_qo_indptr,     // [batch size + 1]
+    const torch::Tensor& pages_kv_indptr,     // [batch size + 1]
+    const int32_t        num_heads_per_head_k,
+    const int32_t        num_heads_k,
+    const bool           is_causal,
+    torch::Tensor&       work_metadata_ptrs,
+    torch::Tensor&       work_indptr,
+    torch::Tensor&       work_info_set,
+    torch::Tensor&       reduce_indptr,
+    torch::Tensor&       reduce_final_map,
+    torch::Tensor&       reduce_partial_map,
+    const int32_t        kv_granularity,
+    const int32_t        max_seqlen_qo,
+    const int32_t        uni_seqlen_qo,
+    const bool           fast_mode,
+    const int32_t        topk,
+    const int32_t        max_split_per_batch)
+{
+    const at::hip::OptionalHIPGuardMasqueradingAsCUDA device_guard(device_of(pages_kv_indptr));
+
+    TORCH_CHECK((kv_granularity & (kv_granularity - 1)) == 0,
+                __func__, ": kv_granularity Must be power of 2!");
+    TORCH_CHECK(seqlens_qo_indptr.stride(0) == 1,
+                __func__, ": seqlens_qo_indptr should be continuous!");
+    TORCH_CHECK(seqlens_qo_indptr.scalar_type() == at::ScalarType::Int,
+                __func__, ": seqlens_qo_indptr's element type should be int!");
+    TORCH_CHECK(pages_kv_indptr.stride(0) == 1,
+                __func__, ": seqlens_kv_indptr should be continuous!");
+    TORCH_CHECK(pages_kv_indptr.scalar_type() == at::ScalarType::Int,
+                __func__, ": seqlens_kv_indptr's element type should be int!");
+
+    get_pa_metadata_v1_2_device(
+        seqlens_qo_indptr,
+        pages_kv_indptr,
+        num_heads_per_head_k,
+        num_heads_k,
+        is_causal,
+        kv_granularity,
+        max_seqlen_qo,
+        uni_seqlen_qo,
+        topk,
+        max_split_per_batch,
+        work_metadata_ptrs,
+        work_info_set,
+        work_indptr,
+        reduce_indptr,
+        reduce_final_map,
+        reduce_partial_map);
+
 }
