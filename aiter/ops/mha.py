fb86833e6f7d3fdfdf5d586fd255046dc2cc99dc
mqhc2020 <marvin.tsai@amd.com>
2025-12-02 10:56:08 +0800

    fmha: swtich to CK FAv2 when soft-capping value is valid (#1519)

--- aiter/ops/mha.py
+++ aiter/ops/mha.py
@@ -1960,6 +1960,7 @@ def _flash_attn_varlen_forward(
         ret = ret and (not swa)
         ret = ret and (q.dtype == dtypes.bf16)
         ret = ret and (cu_seqlens_q_padded is None and cu_seqlens_k_padded is None)
+        ret = ret and logits_soft_cap == 0.0
         return ret
 
     q, k, v = [maybe_contiguous(x) for x in (q, k, v)]
