ea61ea1eaf429c40cf09940ac42464d83b6715b1
amirumoAMD <Amelia.Moore@amd.com>
2025-12-19 11:06:36 -0500

    LLfp4 qr cap for atom (#1673)
    * QR cap implemented to limit QR to prefill
    
    * test git config
    
    * Fix to genericize qr comm cap
    
    * Incorrect cap number

--- aiter/dist/device_communicators/communicator_cuda.py
+++ aiter/dist/device_communicators/communicator_cuda.py
@@ -155,6 +155,8 @@ class CudaCommunicator(DeviceCommunicatorBase):
             qr_comm is not None
             and not qr_comm.disabled
             and qr_comm.should_quick_allreduce(input_)
+            and (input_.nelement() * input_.element_size()) >= 4*1024*1024 # input shape should be such that quick reduce will show benefits.
+            # input shape estimated at 2 * max concurrency for now. if performance issues, subject to change
         ):
             out = qr_comm.quick_all_reduce(input_)
             assert out is not None
