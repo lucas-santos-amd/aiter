151bcbb4c5c1925bc46e4e338559638a08da1777
la <46212055+junhaha666@users.noreply.github.com>
2025-12-19 14:04:11 +0800

    Remove the input parameter "out" in gemm_a4w4 (#1679)
    * Remove the input parameter "out" in gemm_a4w4
    
    * update
    
    * format
    
    ---------
    
    Co-authored-by: valarLip <Lingpeng.Jin@amd.com>

--- csrc/py_itfs_cu/asm_gemm_a4w4.cu
+++ csrc/py_itfs_cu/asm_gemm_a4w4.cu
@@ -113,7 +113,8 @@ std::tuple<std::string, int> get_heuristic_kernel(int M,
         if(cfg.bpreshuffle == bpreshuffle_en &&
            (cfg.splitK >= log2_k_split_en))
         {
-            if((N % cfg.tile_N) == 0)
+            // tile128x512 may not support N % cfg.tile_N != 0
+            if(cfg.tile_M != 128 || cfg.tile_N != 512 || (N % cfg.tile_N) == 0)
             {
                 std::vector<int> splitK_list =
                     (log2_k_split.has_value() && cfg.splitK)
