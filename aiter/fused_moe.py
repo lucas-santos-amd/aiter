8c500c9121d96b8cf77b8871f0241fe9aba5ba1a
zufayu <zufa.yu@amd.com>
2025-12-24 16:23:23 +0800

    add fp32 input (#1706)
    * add fp32 input
    
    * format code
    
    * perf bug fix
    
    * logic fix : out type != input type
    
    * bug fix
    
    * format code
    
    * remove dtype convert before act_and_mul in fused_moe
    
    ---------
    
    Co-authored-by: zufayu <zufayu@amd.com>
    Co-authored-by: chenjun <junchen2@amd.com>

--- aiter/fused_moe.py
+++ aiter/fused_moe.py
@@ -1132,9 +1132,9 @@ def asm_stage1(
     )
     if ksplit > 0:
         if activation == ActivationType.Silu:
-            aiter.silu_and_mul(out, tmp_out.view(dtypes.fp32).to(dtype))
+            aiter.silu_and_mul(out, tmp_out.view(dtypes.fp32))
         else:
-            aiter.gelu_and_mul(out, tmp_out.view(dtypes.fp32).to(dtype))
+            aiter.gelu_and_mul(out, tmp_out.view(dtypes.fp32))
     return out
 
 
@@ -1446,9 +1446,9 @@ def ck_moe_stage1(
     )
     if splitk > 1:
         if activation == ActivationType.Silu:
-            aiter.silu_and_mul(out, tmp_out.view(dtypes.fp32).to(out.dtype))
+            aiter.silu_and_mul(out, tmp_out.view(dtypes.fp32))
         else:
-            aiter.gelu_and_mul(out, tmp_out.view(dtypes.fp32).to(out.dtype))
+            aiter.gelu_and_mul(out, tmp_out.view(dtypes.fp32))
     return out
 
 
