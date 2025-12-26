9ad63a78f72af9582fe1db483a83dd5c7a03d359
Pleaplusone <ygan@amd.com>
2025-12-26 18:08:03 +0800

    add qk_norm_rope_cache_quant fusion (#1650)
    * qk_norm_rope_cache_quant fusion
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * fix acc bug
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * float calculation
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * adopt ck's fp8 type convert
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * v asm layout
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * fix slot mapping load
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * support per token quant
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * fix num_head dim
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * formating code
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * rename the file
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * format
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * format
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * rename
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * remove unnecessary code
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * format cpp/cu file
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * format py files
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * format py files
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * change compileconfig
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * reduce over head_dim only
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * reduce over head_dim only
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * fix ruff and black issue in __init__
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * format py files
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * fix format issue
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    * fp8 dtype fix
    
    Signed-off-by: ganyi <ygan@amd.com>
    
    ---------
    
    Signed-off-by: ganyi <ygan@amd.com>
    Co-authored-by: Zhu Yuhua <yuhzhu@amd.com>

--- csrc/include/rocm_ops.hpp
+++ csrc/include/rocm_ops.hpp
@@ -1445,6 +1445,9 @@ namespace py = pybind11;
     m.def("fused_mrope_3d_rms", &fused_mrope_3d_rms); \
     m.def("fused_rope_rms", &fused_rope_rms);
 
+#define FUSED_QKNORM_ROPE_CACHE_QUANT_PYBIND \
+    m.def("fused_qk_norm_rope_cache_quant_shuffle", &aiter::fused_qk_norm_rope_cache_quant_shuffle);
+
 #define SMOOTHQUANT_PYBIND                      \
     m.def("smoothquant_fwd", &smoothquant_fwd); \
     m.def("moe_smoothquant_fwd", &moe_smoothquant_fwd);
@@ -1635,15 +1638,15 @@ namespace py = pybind11;
           py::arg("final_output"),       \
           py::arg("final_lse") = std::nullopt);
 
-#define TOPK_PLAIN_PYBIND           \
-    m.def("topk_plain",             \
-          &topk_plain,              \
-          py::arg("values"),        \
-          py::arg("topk_ids"),      \
-          py::arg("topk_out"),      \
-          py::arg("topk"),          \
-          py::arg("largest") = true, \
+#define TOPK_PLAIN_PYBIND                         \
+    m.def("topk_plain",                           \
+          &topk_plain,                            \
+          py::arg("values"),                      \
+          py::arg("topk_ids"),                    \
+          py::arg("topk_out"),                    \
+          py::arg("topk"),                        \
+          py::arg("largest")   = true,            \
           py::arg("rowStarts") = torch::Tensor(), \
-          py::arg("rowEnds") = torch::Tensor(), \
-          py::arg("stride0") = -1,  \
-          py::arg("stride1") = 1);
+          py::arg("rowEnds")   = torch::Tensor(), \
+          py::arg("stride0")   = -1,              \
+          py::arg("stride1")   = 1);
