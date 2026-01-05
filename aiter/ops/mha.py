7574e2fd682ad490056a6ed9913ec3f458cf10a8
tom.jen <tomjen12@amd.com>
2026-01-05 21:31:29 +0800

    [Fix] Add mutates_args to flash_attn_backward to fix AOTAutograd DDP … (#1712)
    * [Fix] Add mutates_args to flash_attn_backward to fix AOTAutograd DDP error
    
    * Apply black formatting

--- aiter/ops/mha.py
+++ aiter/ops/mha.py
@@ -1540,7 +1540,9 @@ def _flash_attn_backward_fake(
     return softmax_d
 
 
-@torch_compile_guard(gen_fake=_flash_attn_backward_fake)
+@torch_compile_guard(
+    mutates_args=["dq", "dk", "dv"], gen_fake=_flash_attn_backward_fake
+)
 def _flash_attn_backward(
     dout: torch.Tensor,
     q: torch.Tensor,
