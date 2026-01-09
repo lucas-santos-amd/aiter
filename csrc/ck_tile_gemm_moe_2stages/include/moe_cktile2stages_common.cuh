09cd96fa2aed473543fcbede1b8644fd81a17f85
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2026-01-08 23:50:32 -0800

    maybe fix build (#1786)
    * maybe fix build
    
    * update ck submodule past 3359
    
    * Update to ROCm/composable_kernel@e339101
    
    ---------
    
    Co-authored-by: Ding, Yi <yi.ding@amd.com>

--- csrc/ck_tile_gemm_moe_2stages/include/moe_cktile2stages_common.cuh
+++ csrc/ck_tile_gemm_moe_2stages/include/moe_cktile2stages_common.cuh
@@ -158,12 +158,10 @@ void moe_gemm(const MoeFlatmmHostArgs& args, const ck_stream_config& s)
 
     const auto Run = [&](const auto has_hot_loop_,
                          const auto tail_number_,
-                         const auto memory_operation_,
                          const auto b_mem_nt_type_) {
         constexpr bool has_hot_loop_v   = has_hot_loop_.value;
         constexpr auto tail_number_v    = tail_number_.value;
         constexpr auto scheduler        = FlatmmConfig::Scheduler;
-        constexpr auto memory_operation = memory_operation_.value;
         constexpr auto b_mem_nt_type_v =
             static_cast<ck_tile::amd_buffer_coherence_enum>(b_mem_nt_type_.value);
 
@@ -220,7 +218,6 @@ void moe_gemm(const MoeFlatmmHostArgs& args, const ck_stream_config& s)
                                              FlatmmConfig::N_Warp_Tile,
                                              FlatmmConfig::K_Warp_Tile,
                                              CodegenPipelineProblem::TransposeC,
-                                             memory_operation,
                                              FlatmmConfig::NumWaveGroups,
                                              false,
                                              1,
@@ -326,50 +323,31 @@ void moe_gemm(const MoeFlatmmHostArgs& args, const ck_stream_config& s)
     };
 
     const auto RunBMem =
-        [&](const auto has_hot_loop_, const auto tail_number_, const auto memory_operation_) {
+        [&](const auto has_hot_loop_, const auto tail_number_) {
             switch(b_mem_nt_type)
             {
             case 2: {
                 Run(has_hot_loop_,
                     tail_number_,
-                    memory_operation_,
                     ck_tile::integral_constant<int32_t, 2>{});
             }
             break;
             default: {
                 Run(has_hot_loop_,
                     tail_number_,
-                    memory_operation_,
                     ck_tile::integral_constant<int32_t, 0>{});
             }
             }
         };
 
-    const auto RunSplitk = [&](const auto has_hot_loop_, const auto tail_number_) {
-        if(args.k_batch == 1)
-        {
-            RunBMem(has_hot_loop_,
-                    tail_number_,
-                    ck_tile::integral_constant<ck_tile::memory_operation_enum,
-                                               ck_tile::memory_operation_enum::set>{});
-        }
-        else
-        {
-            RunBMem(has_hot_loop_,
-                    tail_number_,
-                    ck_tile::integral_constant<ck_tile::memory_operation_enum,
-                                               ck_tile::memory_operation_enum::atomic_add>{});
-        }
-    };
-
     if(tail_num == ck_tile::TailNumber::Odd)
     {
-        RunSplitk(ck_tile::bool_constant<true>{},
+        RunBMem(ck_tile::bool_constant<true>{},
                   ck_tile::integral_constant<ck_tile::TailNumber, ck_tile::TailNumber::Odd>{});
     }
     else if(tail_num == ck_tile::TailNumber::Even)
     {
-        RunSplitk(ck_tile::bool_constant<true>{},
+        RunBMem(ck_tile::bool_constant<true>{},
                   ck_tile::integral_constant<ck_tile::TailNumber, ck_tile::TailNumber::Even>{});
     }
     else
