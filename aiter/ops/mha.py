dadacee638f847f7332a81b9b319063ab67ea07e
JaxChen29 <jichen@amd.com>
2026-02-12 11:15:15 +0800

    enable hd192_128 br kernel in python (#2009)

--- aiter/ops/mha.py
+++ aiter/ops/mha.py
@@ -1622,8 +1622,7 @@ def _flash_attn_backward(
         ret &= not deterministic or is_950_1block
         ret &= nhead_q % nhead_k == 0
         ret &= (
-            (hdim_q > 64 and hdim_q <= 128)
-            or (hdim_q == 192 and hdim_v == 128 and nmask)
+            (hdim_q > 64 and hdim_q <= 128) or (hdim_q == 192 and hdim_v == 128)
         ) and hdim_q % 8 == 0
         ret &= not swa
         return ret
