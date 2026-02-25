6ac69078279d1d27942ae918b665bb53c66b949e
minmengdie <memin@amd.com>
2026-02-25 14:50:51 +0800

    upload mla_a8w8_qh64_qseqlen4_gqaratio16 co in MI300 (#2042)
    * upload mla_a8w8_qh64_qseqlen4_gqaratio16_ps.co in MI300
    
    * upload mla_a8w8_qh64_qseqlen4_gqaratio16.co in MI300
    
    * add arch restriction for qh32 case
    
    * add arch judge in mla meta data
    
    ---------
    
    Co-authored-by: Lingpeng Jin <103567126+valarLip@users.noreply.github.com>
    Co-authored-by: Jim, Guo <jim.guo@amd.com>

--- aiter/mla.py
+++ aiter/mla.py
@@ -12,7 +12,7 @@ import triton.language as tl
 
 import aiter
 from aiter import dtypes
-from aiter.jit.utils.chip_info import get_cu_num
+from aiter.jit.utils.chip_info import get_cu_num, get_gfx
 
 
 @triton.jit
@@ -289,7 +289,8 @@ def mla_decode_fwd(
                 nhead == 128 and q.dtype == dtypes.fp8 and kv_buffer.dtype == dtypes.fp8
             )
             or (
-                nhead == 32
+                get_gfx() == "gfx950"
+                and nhead == 32
                 and q.dtype == dtypes.fp8
                 and kv_buffer.dtype == dtypes.fp8
                 and max_seqlen_q == 4
--- csrc/kernels/mla/metadata/v1_2_device.cuh
+++ csrc/kernels/mla/metadata/v1_2_device.cuh
@@ -421,6 +421,8 @@ void get_mla_metadata_v1_2_device(const torch::Tensor& seqlens_qo_indptr, // [ba
     int32_t qk_batch_ratio = 1;
     int32_t uni_seqlen_qo  = ori_uni_seqlen_qo;
 
+    auto arch_id = get_gpu_arch();
+
     // In the following cases, we use #head=16 to simulate cases which is not natively supported by
     // mla main kernel.
     const bool q_is_fp8 =
@@ -428,9 +430,10 @@ void get_mla_metadata_v1_2_device(const torch::Tensor& seqlens_qo_indptr, // [ba
     const bool kv_is_fp8 =
         (kv_dtype == at::ScalarType::Float8_e4m3fnuz || kv_dtype == at::ScalarType::Float8_e4m3fn);
 
-    const bool natively_supported =
-        (num_heads == 16) || ((num_heads == 32) && q_is_fp8 && kv_is_fp8 && (max_seqlen_qo == 4)) ||
-        ((num_heads == 128) && q_is_fp8 && kv_is_fp8);
+    const bool natively_supported = (num_heads == 16) ||
+                                    ((arch_id == "gfx950") && (num_heads == 32) && q_is_fp8 &&
+                                     kv_is_fp8 && (max_seqlen_qo == 4)) ||
+                                    ((num_heads == 128) && q_is_fp8 && kv_is_fp8);
 
     if((natively_supported == false) && (num_heads % 16 == 0))
     {
