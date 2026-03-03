d4f5e52627f57c8a9acb2e65badeb52ffb0552bf
Yuzhen Zhou <82826991+zyzshishui@users.noreply.github.com>
2026-03-02 17:00:45 -0800

    Use unreg path for custom all-reduce during CUDA graph capture (#2075)
    * 1
    
    * 1
    
    * 1

--- aiter/dist/device_communicators/custom_all_reduce.py
+++ aiter/dist/device_communicators/custom_all_reduce.py
@@ -55,6 +55,7 @@ class CustomAllreduce:
         group: ProcessGroup,
         device: Union[int, str, torch.device],
         max_size=8192 * 1024 * 8 * 2, # In allreduce 2stage writemode, use 2x tmp buffer
+        enable_register_for_capturing: bool = True,
     ) -> None:
         """
         Args:
@@ -147,6 +148,7 @@ class CustomAllreduce:
         #     return
 
         self.disabled = False
+        self.enable_register_for_capturing = enable_register_for_capturing
         # buffers memory are owned by this Python class and passed to C++
         # meta data composes of two parts: meta data for synchronization
         # (256 bytes) and a temporary buffer for storing intermediate
@@ -313,8 +315,8 @@ class CustomAllreduce:
                     input,
                     use_new=use_new,
                     open_fp8_quant=open_fp8_quant,
-                    registered_input=True,
-                    registered_output=True
+                    registered_input=self.enable_register_for_capturing,
+                    registered_output=self.enable_register_for_capturing
                 )
             else:
                 # if warm up, mimic the allocation pattern
