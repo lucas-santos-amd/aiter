856b644b061ed8f9697b7fca03c1b8f317b09859
Sergey Solovyev <sergey.solovyev@amd.com>
2026-03-15 01:34:16 +0000

    64x256 fmoe (#2279)
    * add logic to select 64 tokens kernel for large sequence sizes
    
    * modify aiter interface to support 64 tokens
    
    * remove comments
    
    * Update granularity for heuristics. Alwyas prefer the tuned csv
    
    * Add new 64x256 kernel with batched scheduling in XCD
    
    * add gelu kernel
    
    * add gelu 64x256 to config
    
    * remove merge conflict left over
    
    * apply black requiried modifications
    
    * remove debug statement
    
    * add condition to enable 64x256 only for blockscale quants
    
    * enable 64x256 selection only for gfx950
    
    * apply black formatting
    
    ---------
    
    Co-authored-by: Anton Saukkonen <63663359+antsaukk@users.noreply.github.com>
    Co-authored-by: Sergey Solo <ssolovye@amd.com>

--- csrc/include/rocm_ops.hpp
+++ csrc/include/rocm_ops.hpp
@@ -1299,7 +1299,8 @@ namespace py = pybind11;
           py::arg("fc_scale_blkn")    = 128,                                   \
           py::arg("fc_scale_blkk")    = 128,                                   \
           py::arg("fc2_smooth_scale") = std::nullopt,                          \
-          py::arg("activation")       = ActivationType::Silu);                       \
+          py::arg("activation")       = ActivationType::Silu,                        \
+          py::arg("block_size_M")     = 32);                                         \
     m.def("moe_stage1_g1u1",                                                   \
           &moe_stage1_g1u1,                                                    \
           py::arg("input"),                                                    \
